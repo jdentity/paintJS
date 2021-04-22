cf2066b7e9d09e16f0b60ad25e1f50467d736b8a
Jdentity <identityjang@gmail.com>
2021-04-22 15:55:30 +0900

    brush size

--- app.js
+++ app.js
@@ -1,28 +1,69 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+let isIn = false;
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.moveTo(x, y);
+    ctx.beginPath();
+  } else if (painting && isIn) {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function startPainting() {
+  painting = true;
+}
 
 function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-  console.log(x, y);
+function leaveMouse() {
+  isIn = false;
+  ctx.closePath();
 }
 
-function onMouseDown(event) {
-  painting = true;
+function enterMouse() {
+  isIn = true;
+  ctx.beginPath();
 }
 
-function onMouseUp(event) {
-  stopPainting();
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
-if(canvas) {
+function handleRangeChange(event) {
+  const range = event.target.value;
+  ctx.lineWidth = range;
+}
+
+if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  window.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", leaveMouse);
+  canvas.addEventListener("mouseenter", enterMouse);
+}
+
+Array.from(colors).forEach(color => 
+  color.addEventListener("click", handleColorClick)
+);
+
+if(range) {
+  range.addEventListener("input", handleRangeChange);
 }
